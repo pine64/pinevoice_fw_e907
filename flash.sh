9999855ba25bcfd94dc02f1e86d1716de1a7e243
Marek Kraus <gamelaster@outlook.com>
2026-05-10 11:29:23 +0200

    flash: full flash support

--- flash.sh
+++ flash.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 PV_TTY=$(find /dev -maxdepth 1 -type c -name 'tty*' ! -name 'tty' | head -n1)
 
-../../tools/flashtool/bflb_iot_tool-ubuntu --interface=uart --baudrate=2000000 --chipname=bl606p \
+if [ "$2" == "full" ]; then
+    # ../../tools/flashtool2/.venv/bin/python ../../tools/flashtool2/bflb_iot_tool/__main__.py --interface=uart --baudrate=2000000 --chipname=bl606p \
+    ../../tools/flashtool/bflb_iot_tool-ubuntu --interface=uart --baudrate=2000000 --chipname=bl606p \
         --boot2="../../boards/bl606p_pinevoice_e907/bootimgs/boot2_isp_release.bin" \
         --firmware="yoc_rfpa.bin" \
         --pt="../../boards/bl606p_pinevoice_e907/configs/partition.toml" \
+        --dts="/workspace/boards/bl606p_pinevoice_e907/configs/chip_params.dts" \
+        --media="generated/littlefs.bin" \
+        --mfg="../../boards/bl606p_pinevoice_e907/bootimgs/bl606p_mfg_gu_8476f7743.bin" \
         --port ${PV_TTY}
+else
+    ../../tools/flashtool/bflb_iot_tool-ubuntu --interface=uart --baudrate=2000000 --chipname=bl606p \
+            --boot2="../../boards/bl606p_pinevoice_e907/bootimgs/boot2_isp_release.bin" \
+            --firmware="yoc_rfpa.bin" \
+            --pt="../../boards/bl606p_pinevoice_e907/configs/partition.toml" \
+            --port ${PV_TTY}
+fi
 if [ "$1" == "cli" ]; then
     tio ${PV_TTY} -b 2000000 -m INLCRNL
 fi
