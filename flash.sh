24a5fc9bc7bc9e3f79d710df714542e260b4b394
Marek Kraus <gamelaster@outlook.com>
2026-07-11 11:08:44 +0200

    flash.sh: update and add copy eflash_loader_cfg.ini

--- flash.sh
+++ flash.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 PV_TTY=$(find /dev -maxdepth 1 -type c -name 'tty*' ! -name 'tty' | head -n1)
 
+cp ../../boards/bl606p_pinevoice_e907/configs/eflash_loader_cfg.ini ../../tools/flashtool/chips/bl606p/eflash_loader/eflash_loader_cfg.ini
+
 if [ "$2" == "full" ]; then
-    # ../../tools/flashtool2/.venv/bin/python ../../tools/flashtool2/bflb_iot_tool/__main__.py --interface=uart --baudrate=2000000 --chipname=bl606p \
     ../../tools/flashtool/bflb_iot_tool-ubuntu --interface=uart --baudrate=2000000 --chipname=bl606p \
-        --boot2="../../boards/bl606p_pinevoice_e907/bootimgs/boot2_isp_release.bin" \
         --firmware="yoc_rfpa.bin" \
         --pt="../../boards/bl606p_pinevoice_e907/configs/partition.toml" \
         --media="generated/littlefs.bin" \
         --mfg="../../boards/bl606p_pinevoice_e907/bootimgs/bl606p_mfg_gu_8476f7743.bin" \
         --port ${PV_TTY}
 
+    # --boot2="../../boards/bl606p_pinevoice_e907/bootimgs/boot2_isp_release.bin" \
     # --dts="/workspace/boards/bl606p_pinevoice_e907/configs/chip_params.dts" \ # DTS is not required, as we already flash _rfpa.bin
 
 else
